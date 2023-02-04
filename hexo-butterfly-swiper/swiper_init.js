89dcc7ed1504927abcef238cf1a765cb78b1f0d7
iuuuuuuuuuu <67570521+wuwuwuzzzzz@users.noreply.github.com>
2023-02-04 15:48:19 +0800

    Add files via upload

---
+++ hexo-butterfly-swiper/swiper_init.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper('.blog-slider', {
+    passiveListeners: true,
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    autoplay: {
+      disableOnInteraction: false,
+      delay: 3000
+    },
+    mousewheel: true,
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
+swiper.el.onmouseenter = function() {
+swiper.autoplay.stop();
+};
+swiper.el.onmouseleave = function() {
+swiper.autoplay.start();
+}
