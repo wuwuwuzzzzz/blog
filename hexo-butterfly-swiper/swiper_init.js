d3e45065cea165842711fb1502c2d6c5a1c4a8a8
iuuuuuuuuuu <759126349@qq.com>
2024-08-27 12:00:11 +0800

    Update swiper_init.js

--- hexo-butterfly-swiper/swiper_init.js
+++ hexo-butterfly-swiper/swiper_init.js
@@ -1,27 +1,28 @@
 var swiper = new Swiper('.blog-slider', {
-    effect: 'fade',
-    loop: true,
-    autoplay: {
-      disableOnInteraction: false,
-      delay: 3000
-    },
-    resizeObserver: true,
-    on: {
-      resize: function() {
-          this.update(); //窗口变化时，更新Swiper的一些属性，如宽高等
-    }, 
-    pagination: {
-      el: '.blog-slider__pagination',
-      clickable: true,
+  effect: 'fade',
+  loop: true,
+  autoplay: {
+    disableOnInteraction: false,
+    delay: 3000
+  },
+  resizeObserver: true,
+  on: {
+    resize: function () {
+      this.update(); //窗口变化时，更新Swiper的一些属性，如宽高等
     }
-  });
+  },
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true
+  }
+});
 
 var comtainer = document.getElementById('swiper_container');
-  if (comtainer !== null) {
-    comtainer.onmouseenter = function() {
-      swiper.autoplay.stop();
-    };
-    comtainer.onmouseleave = function() {
-      swiper.autoplay.start();
-      }
-  } else {}
+if (comtainer !== null) {
+  comtainer.onmouseenter = function () {
+    swiper.autoplay.stop();
+  };
+  comtainer.onmouseleave = function () {
+    swiper.autoplay.start();
+  }
+}
